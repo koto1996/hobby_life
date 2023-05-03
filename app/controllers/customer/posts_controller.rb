616d7b2939eb9a647820853a20eda5664fa03099
kotoe <k0917.kotty@gmail.com>
2023-05-03 13:44:46 +0000

    [修正]AI

--- app/controllers/customer/posts_controller.rb
+++ app/controllers/customer/posts_controller.rb
@@ -5,16 +5,16 @@ class Customer::PostsController < ApplicationController
   end
 
   def create
-    @post=Post.new(post_params)
-    @post.customer_id=current_customer.id
-    @post.save
-    data = Vision.get_image_data(@post.image)
+    post=Post.new(post_params)
+    post.customer_id=current_customer.id
+    post.save
+    data = Vision.get_image_data(post.image)
    if data.value?("LIKELY") || data.value?("VERY_LIKELY")
-    @post.image.purge
-    @post.destroy
+    post.image.purge
+    post.destroy
     redirect_to new_post_path,notice:"投稿に失敗しました"
    else
-    redirect_to post_path(@post),notice:"投稿に成功しました。"
+    redirect_to post_path(post),notice:"投稿に成功しました。"
    end
   end
 
