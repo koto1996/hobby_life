b857f67271e09606750473fb61a2f7d5f4fbc0fa
kotoe <k0917.kotty@gmail.com>
2023-05-03 14:05:20 +0000

    [修正]AI

--- lib/vision.rb
+++ lib/vision.rb
@@ -34,7 +34,7 @@ module Vision
       response = https.request(request, params)
       response_body = JSON.parse(response.body)
       # APIレスポンス出力
-      if (error = response_body['responses'][0]['error']).present?
+      if (error = response_body['responses'][0]["safeSearchAnnotation"]['error']).present?
         raise error['message']
       else
         response_body["responses"][0]["safeSearchAnnotation"]
