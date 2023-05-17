4a5b60168e76d1fe48507ff9da35cdd007e083f9
kotoe <k0917.kotty@gmail.com>
2023-05-17 13:56:24 +0000

    [Java]Favotite

--- app/controllers/customer/favorites_controller.rb
+++ app/controllers/customer/favorites_controller.rb
@@ -1,16 +1,14 @@
 class Customer::FavoritesController < ApplicationController
   before_action :authenticate_customer!
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_customer.favorites.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = @post.favorites.new(customer_id: current_customer.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_customer.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = @post.favorites.find_by(customer_id: current_customer.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
